ad2256f46a4ca7390956d311205a0c7ecb40469c
unknown <serlinasiregar07@gmail.com>
2024-04-01 22:01:28 +0700

    first-commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,80 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Introduction </template>
+
+    
+    <a href="https://vuejs.org/" target="_blank" rel="noopener"></a>
+  Perkenalkan nama saya Serlina Siregar ,saya biasa dipanggil SERLI,saya kuliah di Universitas Islam Riau,
+  Fakultas Teknik,Prodi Teknik Informatika Semester 
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Pendidikan</template>
+
+    SMA : SMAN 1 Padang Sidimpuan<br>
+    SMP : MtsN 1 Padang Sidimpuan<br>
+    SD  : SD NEGERI 005<br>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener"></a> 
+    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener"></a> 
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener"></a> 
+    <a href="https://www.cypress.io/" target="_blank" rel="noopener"></a> 
+    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
+    </a>
+
+    <br />
+
+     <code></code>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Alamat</template>
+
+    Hangtuah,Pekan baru,Riau
+    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener"></a>
+    <a href="https://router.vuejs.org/" target="_blank" rel="noopener"></a>
+    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener"></a>
+    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener"></a>
+    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener"></a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Hobi</template>
+
+    Olahraga Dan Saya Tertarik dengan OOTD
+    <a href="https://chat.vuejs.org" target="_blank" rel="noopener"></a>
+    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"</a> 
+    <a href="https://news.vuejs.org" target="_blank" rel="noopener"></a>
+    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"></a>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Media Sosial</template>
+
+    @srlsrg.
+    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener"></a>
+  </WelcomeItem>
+</template>
