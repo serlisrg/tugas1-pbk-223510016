5ecb6abcef60fbc5f48e90f3afdedf4623bfa02f
unknown <serlinasiregar07@gmail.com>
2024-04-02 14:36:42 +0700

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/serli.JPEG" width="400" height="400" />
+    <img alt="Vue logo" class="logo" src="./assets/serli.jpeg" width="400" height="400" />
 
     <div class="wrapper">
       <HelloWorld msg="Hai Nama Saya Serlina Siregar!" />
